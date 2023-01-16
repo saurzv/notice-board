3d77bc33e1c9aff70de6933515a7e7e381d28c54
Saurav Raj <saurav.raj5592+git@gmail.com>
2023-01-16 11:50:39 +0530

    Complete main services

--- server/services/main.js
+++ server/services/main.js
@@ -8,19 +8,79 @@ const createNotice = async (req, res) => {
 };
 
 const getAllNotices = async (req, res) => {
-    const allNotices = await Notice.find();
-    res.json(allNotices);
+    const notices = await Notice.find({ createdBy: req.user.id }).sort(
+        "createdAt"
+    );
+    res.status(StatusCodes.OK).json({ notices, count: notices.length });
 };
 const getNotice = async (req, res) => {
-    res.send("one Notice");
+    const {
+        user: { id },
+        params: { id: noticeId },
+    } = req;
+
+    try {
+        const notice = await Notice.findOne({
+            _id: noticeId,
+            createdBy: id,
+        });
+        res.status(StatusCodes.OK).json({ notice });
+    } catch (err) {
+        res.status(StatusCodes.BAD_REQUEST).json({
+            status: "err",
+            message: "No notice found with this id",
+        });
+    }
 };
 
 const updateNotice = async (req, res) => {
-    res.send("Update Notices");
+    const {
+        body: { notice },
+        params: { id: noticeId },
+        user: { id },
+    } = req;
+    if (notice === "") {
+        res.status(StatusCodes.BAD_REQUEST).json({
+            status: "err",
+            message: "notice cannot be empty",
+        });
+        return;
+    }
+    try {
+        const updatedNotice = await Notice.findByIdAndUpdate(
+            { _id: noticeId, createdBy: id },
+            req.body,
+            { new: true, runValidators: true }
+        );
+        res.status(StatusCodes.OK).json({ updatedNotice });
+    } catch (err) {
+        res.status(StatusCodes.BAD_REQUEST).json({
+            status: "err",
+            message: "No notice found with this id",
+        });
+    }
 };
 
 const deleteNotice = async (req, res) => {
-    res.send("Delete Notices");
+    const {
+        params: { id: noticeId },
+        user: { id },
+    } = req;
+    try {
+        const deletedNotice = await Notice.findByIdAndRemove({
+            _id: noticeId,
+            createdBy: id,
+        });
+        res.status(StatusCodes.OK).json({
+            status: "Success",
+            message: "Notice deleted successfully",
+        });
+    } catch (err) {
+        res.status(StatusCodes.BAD_REQUEST).json({
+            status: "err",
+            message: "No notice found with this id",
+        });
+    }
 };
 
 module.exports = {
